3f355d66ef3f64b3890027ae3f66b046e9463ce9
Roy Lu <Roylu0404@gmail.com>
2026-01-11 23:44:48 -0500

    Readme

--- History/src/App.jsx
+++ History/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <>
-      <div style={{ display: 'flex', height: '100vh', width: '100vw', overflow: 'hidden' }}>
+      <div style={{ display: 'flex', height: '100%', width: '100%', overflow: 'hidden' }}>
         <MainPanel 
           isExpanded={expandedIndex === null}
           onClick={() => setExpandedIndex(null)}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # historyculminating
-history culminating
+
+A web-based project analyzing the Top 10 most significant events in Canadian and world history, focusing on historical context, key details, and long-term impact. Built to present  historical concepts through clear structure and careful analysis.
+
